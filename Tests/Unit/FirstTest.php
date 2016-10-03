f3911ba14ef53c3a69186c9f7b27d947f296bfa7
Romain Canon <romain.hydrocanon@gmail.com>
2016-10-03 13:51:33 +0200

    [TASK] Implement Travis & Coverrals integration (#1)
    Creating base configuration for Travis to check every build. Also bind Coverrals to see the coverage progress.
    
    Unit tests integration work for the whole core will begin after that.

---
+++ Tests/Unit/FirstTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Romm\Formz\Tests\Unit;
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+class FirstTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     */
+    public function firstTest()
+    {
+        $this->assertEquals(true, true);
+    }
+}
