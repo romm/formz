9df9e5946befd012d0788c3c73e34dee6ded9a43
Romain Canon <romain.hydrocanon@gmail.com>
2016-10-10 13:58:36 +0200

    [TASK] Add unit test for `AssetHandlerFactory.php`

---
+++ Tests/Unit/AssetHandler/AssetHandlerFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Romm\Formz\Tests\Unit\AssetHandler;
+
+use Romm\Formz\AssetHandler\AssetHandlerFactory;
+use Romm\Formz\Tests\Unit\AbstractUnitTest;
+use TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext;
+
+class AssetHandlerFactoryTest extends AbstractUnitTest
+{
+
+    /**
+     * Checks that the asset handler factory is correctly initialized, and that
+     * its getters work properly.
+     *
+     * Will also check that getting two time a factory for a given object will
+     * return the same instance.
+     *
+     * @test
+     */
+    public function assetHandlerFactoryIsInitializedCorrectly()
+    {
+        $formObject = $this->getFormObject();
+        $controllerContext = new ControllerContext;
+
+        $assetHandlerFactory = AssetHandlerFactory::get($formObject, $controllerContext);
+
+        $this->assertSame($formObject, $assetHandlerFactory->getFormObject());
+        $this->assertSame($controllerContext, $assetHandlerFactory->getControllerContext());
+
+        $assetHandlerFactory2 = AssetHandlerFactory::get($formObject, $controllerContext);
+
+        $this->assertSame($assetHandlerFactory, $assetHandlerFactory2);
+    }
+}
