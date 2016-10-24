5a92aae31b705bb7c3cd810eb0025e792884a19b
Romain Canon <romain.hydrocanon@gmail.com>
2016-10-24 17:28:00 +0200

    [TASK] Add unit test for `ErrorContainerDisplayCssAssetHandler`

---
+++ Tests/Unit/AssetHandler/AbstractAssetHandlerTestClass.php
@@ -0,0 +1,19 @@
+<?php
+namespace Romm\Formz\Tests\Unit\AssetHandler;
+
+use Romm\Formz\Tests\Unit\AbstractUnitTest;
+
+class AbstractAssetHandlerTestClass extends AbstractUnitTest
+{
+
+    /**
+     * Returns the same string, but without any space/tab/new line.
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function trimString($string)
+    {
+        return preg_replace('/\s+/', '', $string);
+    }
+}
