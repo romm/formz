f6d98f7c135404a542e554c1a1614ba35bd3c9fb
Romain Canon <romain.hydrocanon@gmail.com>
2016-10-24 18:37:40 +0200

    [TASK] Add Unit tests for `ErrorContainerDisplayCssAssetHandler`  (#16)

---
+++ Tests/Unit/AssetHandler/Css/ErrorContainerDisplayCssAssetHandlerTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Romm\Formz\Tests\Unit\AssetHandler\Css;
+
+use Romm\Formz\AssetHandler\AssetHandlerFactory;
+use Romm\Formz\AssetHandler\Css\ErrorContainerDisplayCssAssetHandler;
+use Romm\Formz\Core\Core;
+use Romm\Formz\Tests\Fixture\Form\DefaultForm;
+use Romm\Formz\Tests\Unit\AssetHandler\AbstractAssetHandlerTestClass;
+use TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext;
+
+class ErrorContainerDisplayCssAssetHandlerTest extends AbstractAssetHandlerTestClass
+{
+
+    /**
+     * Checks that the CSS code returned by the asset handler is valid.
+     *
+     * @test
+     */
+    public function errorContainerDisplayCssIsValid()
+    {
+        $expectedCss = 'form[name="foo"]:not([formz-error-foo="1"])[formz-field-feedback-container="foo"]{display:none!important;}';
+
+        $form = Core::get()->getFormObjectFactory()->getInstanceFromClassName(DefaultForm::class, 'foo');
+        $controllerContext = new ControllerContext();
+        $assetHandlerFactory = AssetHandlerFactory::get($form, $controllerContext);
+
+        $errorContainerDisplayCss = ErrorContainerDisplayCssAssetHandler::with($assetHandlerFactory)
+            ->getErrorContainerDisplayCss();
+
+        $this->assertEquals($this->trimString($errorContainerDisplayCss), $expectedCss);
+    }
+}
