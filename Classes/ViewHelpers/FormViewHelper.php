5f119e1be9b7510bd79378539ad02d7831cb0b15
Romain Canon <romain.hydrocanon@gmail.com>
2017-01-25 10:13:29 +0100

    [TASK] Introduce legacy version of `FormViewHelper`
    This commit does the same as 97b745e91c445e407cdaa3353c46fbb266c99a40 which was
    not implemented in master yet.
    
    We need this fix now because it can throw a warning depending on the current
    TYPO3 version.

--- Classes/ViewHelpers/FormViewHelper.php
+++ Classes/ViewHelpers/FormViewHelper.php
@@ -139,32 +139,9 @@ class FormViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\FormViewHelper
     }
 
     /**
-     * Render the form.
-     *
-     * @param string  $action                               Target action
-     * @param array   $arguments                            Arguments
-     * @param string  $controller                           Target controller
-     * @param string  $extensionName                        Target Extension Name (without "tx_" prefix and no underscores). If NULL the current extension name is used
-     * @param string  $pluginName                           Target plugin. If empty, the current plugin name is used
-     * @param int $pageUid                              Target page uid
-     * @param mixed   $object                               Object to use for the form. Use in conjunction with the "property" attribute on the sub tags
-     * @param int $pageType                             Target page type
-     * @param bool $noCache                              set this to disable caching for the target page. You should not need this.
-     * @param bool $noCacheHash                          set this to suppress the cHash query parameter created by TypoLink. You should not need this.
-     * @param string  $section                              The anchor to be added to the action URI (only active if $actionUri is not set)
-     * @param string  $format                               The requested format (e.g. ".html") of the target page (only active if $actionUri is not set)
-     * @param array   $additionalParams                     additional action URI query parameters that won't be prefixed like $arguments (overrule $arguments) (only active if $actionUri is not set)
-     * @param bool $absolute                             If set, an absolute action URI is rendered (only active if $actionUri is not set)
-     * @param bool $addQueryString                       If set, the current query parameters will be kept in the action URI (only active if $actionUri is not set)
-     * @param array   $argumentsToBeExcludedFromQueryString arguments to be removed from the action URI. Only active if $addQueryString = TRUE and $actionUri is not set
-     * @param string  $fieldNamePrefix                      Prefix that will be added to all field names within this form. If not set the prefix will be tx_yourExtension_plugin
-     * @param string  $actionUri                            can be used to overwrite the "action" attribute of the form tag
-     * @param string  $objectName                           name of the object that is bound to this form. If this argument is not specified, the name attribute of this form is used to determine the FormObjectName
-     * @param string  $hiddenFieldClassName
-     * @return string rendered form
-     * @throws \Exception
+     * @return string
      */
-    public function render($action = null, array $arguments = [], $controller = null, $extensionName = null, $pluginName = null, $pageUid = null, $object = null, $pageType = 0, $noCache = false, $noCacheHash = false, $section = '', $format = '', array $additionalParams = [], $absolute = false, $addQueryString = false, array $argumentsToBeExcludedFromQueryString = [], $fieldNamePrefix = null, $actionUri = null, $objectName = null, $hiddenFieldClassName = null)
+    protected function renderViewHelper()
     {
         $this->timeTracker = TimeTracker::getAndStart();
         $result = '';
--- ext_localconf.php
+++ ext_localconf.php
@@ -19,5 +19,21 @@ call_user_func(
         }
 
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = \Romm\Formz\Core\Core::class . '->clearCacheCommand';
+
+        /** @var \TYPO3\CMS\Extbase\Object\Container\Container $container */
+        $container = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class);
+        $typo3Version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version();
+
+        if (version_compare($typo3Version, '8.3.0', '<')) {
+            $container->registerImplementation(\Romm\Formz\ViewHelpers\FormViewHelper::class, \Romm\Formz\ViewHelpers\Legacy\OldFormViewHelper::class);
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Romm\Formz\ViewHelpers\FormViewHelper::class] = [
+                'className' => \Romm\Formz\ViewHelpers\Legacy\OldFormViewHelper::class
+            ];
+        } else {
+            $container->registerImplementation(\Romm\Formz\ViewHelpers\FormViewHelper::class, \Romm\Formz\ViewHelpers\Legacy\FormViewHelper::class);
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Romm\Formz\ViewHelpers\FormViewHelper::class] = [
+                'className' => \Romm\Formz\ViewHelpers\Legacy\FormViewHelper::class
+            ];
+        }
     }
 );
